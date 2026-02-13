88f15347e4d7400d233f07a44d091bbdbf931704
FellypeGC <gabrielfellypesteve@gmail.com>
2026-02-12 23:36:30 -0300

    feat: add form components

--- frontend/src/components/FormScreen.tsx
+++ frontend/src/components/FormScreen.tsx
@@ -1,34 +1,56 @@
 import RegistrationForm from "./RegistrationForm"
-
+import { useState } from "react"
 
 const FormScreen = () => {
+  const [isFormVisible, setIsFormVisible] = useState<boolean>(false);
+  const [matricula, setMatricula] = useState<string>("");
+
+  const generateSecureMatricula = () => {
+    const array = new Uint32Array(1);
+    window.crypto.getRandomValues(array);
+    // Garante que o número esteja no range de 7 dígitos
+    return (1000000 + (array[0] % 9000000)).toString();
+  };
+
+  const handleStart = () => {
+    // Generates new registration number
+    const newMatricula = generateSecureMatricula();
+    setMatricula(newMatricula);
+
+    // Changes the state to "true" and shows the form
+    setIsFormVisible(true);
+  }
+
   return (
     <main className="w-full max-w-2xl">
       {/* <!-- Tela Inicial: Botão de Iniciar --> */}
-        <div id="startScreen" className="glass-card p-10 text-center">
-          <p className="text-gray-600 mb-8 text-lg">
-            Clique no botão abaixo para iniciar uma nova inscrição e gerar uma matrícula única para o aluno.
-          </p>
-          <button className="btn-gold w-full py-4 rounded-xl text-xl font-semibold shadow-md cursor-pointer uppercase"> 
-            Iniciar nova inscrição
-          </button>
-          <div className="mt-8 text-xs text-gray-400 uppercase tracking-widest">
-            <p>Grêmio Recreativo Escola de Samba União de Sepetiba</p>
-            <p>Sistema interno de controle de matrículas</p>
-          </div>
-        </div>
-
-        {/* <!-- Tela de Formulário (Oculta inicialmente) --> */}
-        <div id="formScreen" className="glass-card p-8 hidden">
-          <div className="flex justify-between items-center mb-6 border-b pb-4">
-            <h2 className="text-2xl font-bold text-green-700">Novo Cadastro de Aluno</h2>
-            <button className="text-gray-400 hover:text-red-500"> {/* onClick={resetView}  */}
-              Cancelar
+        {/* If one didn't start: shows the initial screen */}
+        {!isFormVisible ? (
+          <div className="glass-card p-10 text-center">
+            <p className="text-gray-600 mb-8 text-lg">
+              Clique no botão abaixo para iniciar uma nova inscrição e gerar uma matrícula única para o aluno.
+            </p>
+            <button className="btn-gold w-full py-4 rounded-xl text-xl font-semibold shadow-md cursor-pointer uppercase" onClick={handleStart}> 
+              Iniciar nova inscrição
             </button>
+            <div className="flex flex-col gap-2 mt-8 text-xs text-gray-400 uppercase tracking-widest">
+              <p>Sistema interno de controle de matrículas</p>
+              <p>Instituto Social Neném Brandão</p>
+            </div>
           </div>
-          <RegistrationForm />
-        </div>
-
+          ) : (
+            // If started: shows the form
+            <div className="glass-card p-8 duration-300">
+              <div className="flex justify-between items-center mb-6 border-b pb-4">
+                <h2 className="text-2xl font-bold text-green-700">Novo Cadastro de Aluno</h2>
+                <button className="text-gray-400 hover:text-red-500 cursor-pointer uppercase" onClick={() => setIsFormVisible(false)}>
+                  Cancelar
+                </button>
+              </div>
+              <RegistrationForm matricula={matricula} />
+            </div>
+          )
+        }
 
         {/* <!-- Mensagem de Sucesso --> */}
         <div id="successMessage" className="glass-card p-10 text-center hidden">
--- frontend/src/components/RegistrationForm.tsx
+++ frontend/src/components/RegistrationForm.tsx
@@ -1,64 +1,191 @@
-import { useForm } from "react-hook-form";
+import { useForm, type SubmitHandler } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 
+// TODO(FIX): fix input types, validations and layout size
 
-const RegistrationForm = () => {
-  
-  const generateMatricula = () => Math.floor(1000000000 * Math.random() * 9000000000).toString();
+type RegistrationFormProps = {
+  matricula: string;
+}
+
+type Inputs = {
+  nome: string;
+  email: string;
+  telefone: string;
+  dataNascimento: unknown; 
+  cpf: string;
+  rg: string;
+  nacionalidade: string;
+}
+
+const calculateAge = (dataNasc: Date) => {
+  const today = new Date();
+  let age = today.getFullYear() - dataNasc.getFullYear();
+  const month = today.getMonth() - dataNasc.getMonth();
+  if (month < 0 || (month === 0 && today.getDate() < dataNasc.getDate())) {
+    age--;
+  }
+  return age >= 18;
+};
+
+const schema = yup.object({
+  nome: yup
+    .string()
+    .min(3)
+    .max(100)
+    .required("Nome é obrigatório"),
+  email: yup
+    .string()
+    .email("Email inválido"),
+  telefone: yup
+    .string()
+    .matches(/^\(\d{2}\) \d{5}-\d{4}$/, "Telefone inválido")
+    .required("Telefone é obrigatório"),
+  dataNascimento: yup
+    .date()
+    .required("Data de nascimento é obrigatória")
+    .test("maior-de-idade", "Você precisa ter 18 anos", (value) => calculateAge(new Date(value))),
+  cpf: yup
+    .string()
+    .matches(/^\d{3}\.\d{3}\.\d{3}-\d{2}$/, "CPF inválido")
+    .required("CPF é obrigatório"),
+  rg: yup
+    .string(),
+  nacionalidade: yup
+    .string()
+});
+
+const RegistrationForm = ({ matricula }: RegistrationFormProps) => {
+  const { 
+    register, 
+    formState: {
+      errors,
+      isSubmitting
+    },
+    handleSubmit, 
+    // watch,
+  } = useForm<Inputs>({
+    resolver: yupResolver(schema),
+  });
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
 
   return (
-    <form id="registrationForm" className="space-y-6"> {/* onSubmit={handleFormSubmit} */}
+    <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
       {/* <!-- Campo de Matrícula (Desabilitado) --> */}
       <div className="bg-blue-50 p-4 rounded-lg border border-blue-100">
         <label className="block text-sm font-semibold text-blue-800 mb-1">MATRÍCULA GERADA (AUTOMÁTICO):</label>
         <input 
-        type="text" id="matriculaInput" disabled 
+        type="text" id="matriculaInput" disabled value={matricula}
         className="form-input w-full p-3 border-none text-2xl tracking-widest text-center rounded bg-transparent" />
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        {/* NOME DO ALUNO */}
         <div className="col-span-1 md:col-span-2">
-          <label className="block text-sm font-medium text-gray-700">Nome Completo *</label>
-          <input type="text" required placeholder="Seu nome completo" className="w-full p-3 border rounded-lg focus:ring-2 focus:ring-green-500 outline-none" />
+          <label className="block text-sm font-medium text-gray-700">
+            Nome Completo
+            <span className="text-red-600">*</span>
+          </label>
+          <input 
+            type="text" 
+            placeholder="Digite seu nome completo"
+            className="w-full p-3 border rounded-lg focus:ring-2 focus:ring-green-500 outline-none" 
+            {...register("nome")} 
+          />
+          <span className="text-red-600">{errors.nome?.message}</span>
         </div>
 
-          <div>
-            <label className="block text-sm font-medium text-gray-700">Email *</label>
-            <input type="email" required placeholder="seu.email@exemplo.com" className="w-full p-3 border rounded-lg focus:ring-2 focus:ring-green-500 outline-none" />
-          </div>
+        <div>
+          <label className="block text-sm font-medium text-gray-700">
+            Email
+          </label>
+          <input 
+            type="email"
+            placeholder="seu.email@exemplo.com" 
+            className="w-full p-3 border rounded-lg focus:ring-2 focus:ring-green-500 outline-none" 
+            {...register("email")}
+          />
+          <span className="text-red-600">{errors.email?.message}</span>
+        </div>
 
-          <div>
-            <label className="block text-sm font-medium text-gray-700">Telefone *</label>
-            <input type="text" required placeholder="(XX) XXXXX-XXXX" className="w-full p-3 border rounded-lg focus:ring-2 focus:ring-green-500 outline-none" />
-          </div>
+        <div>
+          <label className="block text-sm font-medium text-gray-700">
+            Telefone 
+            <span className="text-red-600">*</span>
+          </label>
+          <input 
+            type="text"
+            placeholder="(XX) XXXXX-XXXX" 
+            className="w-full p-3 border rounded-lg focus:ring-2 focus:ring-green-500 outline-none"
+            {...register("telefone")}
+          />
+          <span className="text-red-600">{errors.telefone?.message}</span>
+        </div>
 
-          <div>
-            <label className="block text-sm font-medium text-gray-700">Data de Nascimento *</label>
-            <input type="date" required className="w-full p-3 border rounded-lg focus:ring-2 focus:ring-green-500 outline-none" />
-          </div>
+        {/* DATA DE NASCIMENTO */}
+        <div>
+          <label className="block text-sm font-medium text-gray-700">
+            Data de Nascimento
+            <span className="text-red-600">*</span>
+          </label>
+          <input 
+            type="date"
+            className="w-full p-3 border rounded-lg focus:ring-2 focus:ring-green-500 outline-none"
+            {...register("dataNascimento")}
+          />
+          <span className="text-red-600">{errors.dataNascimento?.message}</span>
+        </div>
 
-          <div>
-            <label className="block text-sm font-medium text-gray-700">CPF *</label>
-            <input type="text" required placeholder="000.000.000-00" className="w-full p-3 border rounded-lg focus:ring-2 focus:ring-green-500 outline-none" />
-          </div>
-      </div>
+        {/* CPF */}
+        <div>
+          <label className="block text-sm font-medium text-gray-700">
+            {/* Optional for underagers - Required for adults */}
+            CPF
+            <span className="text-red-600">*</span>
+          </label>
+          <input 
+            type="text"
+            placeholder="123.456.789-10" 
+            className="w-full p-3 border rounded-lg focus:ring-2 focus:ring-green-500 outline-none"
+            {...register("cpf")}
+          />
+          <span className="text-red-600">{errors.cpf?.message}</span>
+        </div>
 
-      <div>
-        <label className="block text-sm font-medium text-gray-700">Experiência com Samba *</label>
-        <select required className="w-full p-3 border rounded-lg focus:ring-2 focus:ring-green-500 outline-none bg-white">
-          <option value="">Selecione sua experiência</option>
-          <option value="iniciante">Iniciante</option>
-          <option value="intermediario">Intermediário</option>
-          <option value="avancado">Já desfilou antes</option>
-        </select>
+        {/* RG */}
+        <div>
+          <label className="block text-sm font-medium text-gray-700">
+            RG
+          </label>
+          <input 
+            type="text"
+            placeholder="Digite seu RG" 
+            className="w-full p-3 border rounded-lg focus:ring-2 focus:ring-green-500 outline-none"
+            {...register("rg")}
+          />
+          <span className="text-red-600">{errors.rg?.message}</span>
+        </div>
+
+        {/* NACIONALIDADE */}
+        <div>
+          <label className="block text-sm font-medium text-gray-700">
+            Nacionalidade
+          </label>
+          <input 
+            type="text"
+            placeholder="Digite sua nacionalidade" 
+            className="w-full p-3 border rounded-lg focus:ring-2 focus:ring-green-500 outline-none"
+            {...register("nacionalidade")}
+          />
+          <span className="text-red-600">{errors.nacionalidade?.message}</span>
+        </div>
       </div>
 
       <button 
         type="submit" 
         className="w-full bg-green-600 hover:bg-green-700 text-white py-4 rounded-xl font-bold text-lg shadow-lg transition-all"
       >
-        Finalizar Cadastro
+        {isSubmitting ? "Finalizando Cadastro..." : "Finalizar Cadastro"} 
       </button>
     </form>
   )
