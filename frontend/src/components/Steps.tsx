0298da941aeefffa8a439a3f680c050f945051dd
FellypeGC <gabrielfellypesteve@gmail.com>
2026-03-02 16:36:36 -0300

    fix step index colors

--- frontend/src/components/Steps.tsx
+++ frontend/src/components/Steps.tsx
@@ -8,15 +8,16 @@ const Steps = ({ currentStep }: StepsProps) => {
   return (
     <div className="flex justify-center items-center gap-3 py-6">
       {steps.map((step, index) => (
-        <>
-          <div key={step} className={`w-8 h-8 rounded-full flex items-center justify-center font-bold text-sm transition-all duration-300 ${currentStep === step - 1 ? "bg-blue-600 text-white" : "text-gray-400 bg-gray-200"}`}>
-            {currentStep > step ? "" : step}
+        <div key={step} className="flex items-center gap-3">
+
+          <div className={`w-8 h-8 rounded-full flex items-center justify-center font-bold text-sm transition-all duration-300 ${currentStep > index ? "bg-blue-600 text-white" : "text-gray-400 bg-gray-200"}`}> {/* bg-blue-600 or bg-green-600 */}
+            {currentStep > index ? "✓" : step}
           </div>
 
           {index < steps.length - 1 && (
-            <div className={`w-8 h-0.5 transition-colors ${currentStep > step ? "bg-green-500" : "bg-gray-200"}`}></div>
-          )}
-        </>
+            <div className={`w-8 h-0.5 transition-colors ${currentStep > index ? "bg-blue-600" : "bg-gray-200"}`} /> 
+          )} {/* bg-blue-600 or bg-green-600 */}
+        </div>
       ))}
     </div>
   )
