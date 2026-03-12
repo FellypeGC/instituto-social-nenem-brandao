5572d4501422fcf94f6cefff84bcbea71aac9a72
FellypeGC <gabrielfellypesteve@gmail.com>
2026-03-12 10:55:42 -0300

    removed maskCPF comments

--- frontend/utils/masks.ts
+++ frontend/utils/masks.ts
@@ -14,10 +14,10 @@ export const maskCPF = (value: string) => {
   if (!value) return "";
 
   const digits = value
-    .replace(/\D/g, "") // Remove tudo que não é número
-    .replace(/(\d{3})(\d)/, "$1.$2") // 000.0
-    .replace(/(\d{3})(\d)/, "$1.$2") // 000.000.0
-    .replace(/(\d{3})(\d{1,2})/, "$1-$2") // 000.000.000-00
+    .replace(/\D/g, "")
+    .replace(/(\d{3})(\d)/, "$1.$2")
+    .replace(/(\d{3})(\d)/, "$1.$2")
+    .replace(/(\d{3})(\d{1,2})/, "$1-$2")
     .replace(/(-\d{2})\d+?$/, "$1");
   
   return digits;
